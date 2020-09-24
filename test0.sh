7770d6f8667265f421b2d94604a9b350a205b4f5
Edson Walter Noe Valenzuela Hernandez <EdsonV@tecniseguros.com>
2020-09-24 11:17:03 -0600

    unit test lecture 7

---
+++ test0.sh
@@ -0,0 +1,4 @@
+python -c "from test0 import test_prime; test_prime(1,False)"
+python -c "from test0 import test_prime; test_prime(2,True)"
+python -c "from test0 import test_prime; test_prime(3,True)"
+python -c "from test0 import test_prime; test_prime(5,False)"
